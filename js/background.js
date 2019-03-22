909daf677ce21ec5296d0d98222e8d7fb55286e6
hakanonymos <30985149+hakanonymos@users.noreply.github.com>
2019-03-23 00:06:37 +0100

    Update background.js

--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@
 chrome.runtime.onInstalled.addListener(function(details) {
   switch (details.reason) {
     case "install":
-      chrome.tabs.create({url: "https://www.youtube.com/watch?v=UQ2UOsGUQpM&t=256s"});
+      chrome.tabs.create({url: "https://www.youtube.com/watch?v=88em2rWPx_o"});
       break;
     default:
       return true;
